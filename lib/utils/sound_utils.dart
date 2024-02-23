47bb712f2d9a785216103a654b608c6499ee7f6a
Aashman15 <rojinathing@Things-MacBook-Air.local>
2024-02-23 12:08:52 +0545

    edited sound_utils

--- lib/utils/sound_utils.dart
+++ lib/utils/sound_utils.dart
@@ -2,14 +2,16 @@ import 'package:assets_audio_player/assets_audio_player.dart';
 
 final AssetsAudioPlayer _player = AssetsAudioPlayer.newPlayer();
 
+const _soundPathPrefix = 'assets/sounds';
+
 const _soundPaths = {
-  MySounds.roll: 'assets/sounds/dice-roll.mp3',
-  MySounds.move: 'assets/sounds/piece-move.mp3',
-  MySounds.kill: 'assets/sounds/slice.mp3',
-  MySounds.error: 'assets/sounds/error-sound.mp3',
-  MySounds.enterHome: 'assets/sounds/enter-home.mp3',
-  MySounds.congratulations: 'assets/sounds/congratulations.mp3',
-  MySounds.rolledOneThrice: 'assets/sounds/rolled-one-thrice.mp3',
+  MySounds.roll: '$_soundPathPrefix/dice-roll.mp3',
+  MySounds.move: '$_soundPathPrefix/piece-move.mp3',
+  MySounds.kill: '$_soundPathPrefix/slice.mp3',
+  MySounds.error: '$_soundPathPrefix/error-sound.mp3',
+  MySounds.enterHome: '$_soundPathPrefix/enter-home.mp3',
+  MySounds.congratulations: '$_soundPathPrefix/congratulations.mp3',
+  MySounds.rolledOneThrice: '$_soundPathPrefix/rolled-one-thrice.mp3',
 };
 
 void playSound(MySounds sound) {
